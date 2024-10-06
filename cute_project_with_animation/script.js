1d6ea07b6be98d6c0eab7eaab502017f7efacf10
Jenry24 <152663649+Jenry24@users.noreply.github.com>
2024-10-06 21:07:26 +0800

    Delete cute_project_with_animation directory

--- cute_project_with_animation/script.js
+++
@@ -1,5 +0,0 @@
-function showTeddy() {
-    const teddyContainer = document.getElementById('teddy-container');
-    teddyContainer.classList.remove('hidden');
-    document.getElementById('bg-music').play();
-}
