c9557714fda22d126c3633369bba92112f194b06
Jenry24 <152663649+Jenry24@users.noreply.github.com>
2024-10-06 20:48:33 +0800

    Add files via upload

---
+++ cute_project_with_animation/script.js
@@ -0,0 +1,5 @@
+function showTeddy() {
+    const teddyContainer = document.getElementById('teddy-container');
+    teddyContainer.classList.remove('hidden');
+    document.getElementById('bg-music').play();
+}
